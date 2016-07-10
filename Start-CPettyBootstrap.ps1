160b079af13d98b2e5acd5500ed4d5613fe073dd
chris <chris@planetexpress.local>
2016-07-10 13:43:02 -0700

    Getting ready on a new PC

---
+++ Start-CPettyBootstrap.ps1
@@ -0,0 +1,39 @@
+﻿#CPetty ComputerBoostrap
+
+#Enable Hyper-V
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
+
+#Set Powershell Taskbar Customization
+$PSonWinXPath = "HKCU:\software\microsoft\windows\currentversion\explorer\Advanced\"
+$PSonWinXName = "DontUsePowershellonWinX"
+if(get-itemproperty $PSonWinXPath -Name $PSonWinXName -verbose | select -Expand DontUsePowerShellOnWinX){set-itemproperty $PSonWinXPath -Name $PSonWinXName -Value 1 -verbose}
+
+#Set ExecutionPolicy
+set-executionpolicy -scope localmachine -executionpolicy remotesigned
+
+#Download Chocolatey Installer
+iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+
+#List of Commonly downloaded Apps
+$Apps = "googlechrome",
+	    "notepadplusplus.install",
+	    "flashplayerplugin",
+	    "flashplayeractivex",
+	    "7zip.install",
+	    "vlc",
+	    "keepass.install",
+	    "putty.install",
+	    "winscp.install",
+	    "windirstat",
+	    "pester",
+	    "sourcetree",
+	    "greenshot",
+	    "linqpad5.install",
+	    "steam",
+	    "spotify",
+	    "f.lux",
+	    "everything",
+	    "logparser",
+	    "rsat"
+	
+CInst -y $Apps
